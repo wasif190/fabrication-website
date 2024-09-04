d4946ca65944488549dd24bb5c9b05599bad9ada
Wasif <wasif19015@gmail.com>
2024-09-04 13:26:04 +0530

    fixed

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,7 +8,7 @@ function Home() {
         <section className='absolute top-0 left-0 right-0 z-20 bg-[#222] bg-opacity-10 backdrop-blur-lg border-b border-white/30'>
             <Navbar/>
         </section>
-        <section className='z-10 text-white px-[8%] lg:w-[70%] md:w-[80%] lg:-mt-[50px] md:-mt-[150px] -mt-[300px]'> 
+        <section className='z-10 text-white px-[8%] lg:w-[70%] md:w-[80%] lg:-mt-[50px] md:-mt-[150px] -mt-[200px]'> 
             <h1 className='xl:text-[4rem] lg:text-[3.5rem] sm:text-[3rem] mobile:text-[2.8rem] text-[2.4rem] font-bold xl:leading-[4.6rem] lg:leading-[4rem] sm:leading-[4rem] mobile:leading-[3.5rem] leading-[3rem]'>
                 Welding and Metal Fabrication for Every Home
             </h1>
@@ -19,7 +19,7 @@ function Home() {
                 Contact
             </button>
         </section>
-        <div className='md:w-screen mobile:w-[200vw] w-[300vw] right-0 absolute bottom-0'>
+        <div className='md:w-screen mobile:w-[200vw] w-[250vw] right-0 absolute bottom-0'>
             <img src={Hero}/>
         </div>
     </main>
