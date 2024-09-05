b19cc177316fa59640c9250f9b765d9b0248184c
Wasif <wasif19015@gmail.com>
2024-09-05 12:55:36 +0530

    hero-ui

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -6,8 +6,7 @@ import { Link } from 'react-scroll';
 function Home() {
   return (
     <main id='home' className='bg-[#222] md:px-[8%] px-[4%] h-screen flex items-center justify-between relative'>
-        <section className='z-10 text-white lg:w-[70%] md:w-[80%] lg:-mt-[50px] md:-mt-[150px] -mt-[220px]
-            xl:pt-3 lg:pt-[2rem] md:pt-[8rem] pt-[2rem]'
+        <section className='z-10 text-white lg:w-[70%] md:w-[80%] lg:-mt-[50px] md:-mt-[150px] -mt-[240px]'
         > 
             <h1 className='xl:text-[4rem] font-josefin sm:text-[3rem] mobile:text-[2.8rem] text-[2.5rem] font-bold xl:leading-[4.6rem] 
                 lg:leading-[4rem] mobile:leading-[3.5rem] leading-[3.3rem] 
@@ -31,10 +30,11 @@ function Home() {
                     <i className="uil uil-message"></i>
                 </button>
             </Link>
+
+            <div className='absolute -z-10 right-0 bottom-0 md:w-[600px] sm:w-[500px] w-[380px] animated-image'>
+                <img src={Hero}/>
+            </div>
         </section>
-        <div className='absolute right-0 bottom-0 md:w-[600px] sm:w-[500px] w-[380px] animated-image'>
-            <img src={Hero}/>
-        </div>
     </main>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -15,91 +15,93 @@ function Navbar() {
 
     return (
         <nav className='py-4 md:px-[8%] px-[4%] text-white bg-[#222] border-b border-white/30 font-monsterrat flex justify-between 
-            items-center text-[1.05rem] relative'
+            items-center text-[1.05rem] fixed z-20 top-0 left-0 right-0'
         >
             <header className='w-[100px]'>
                 <img src={Logo}/>
             </header>
 
-            <ul id='navbar' className={`items-center gap-[3rem] lg:flex
-                ${isNavActive ? 'absolute z-10 w-full top-[5.1rem] border-t border-b border-white/30 bg-[#222] left-0 p-7 pb-14 px-[4%] lg:px-[10%] grid grid-cols-3 rounded-b-xl transition-all duration-300': 'hidden'}`}
-            >
-                <Link
-                    onClick={closeMenu}
-                    activeClass="navbar--active-content"
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration={400}
-                    to="home"
-                    className="navbar--content"
+            <div>
+                <ul id='navbar' className={`items-center gap-[3rem] lg:flex
+                    ${isNavActive ? 'absolute z-10 w-full top-[5.1rem] border-t border-b border-white/30 bg-[#222] left-0 p-7 pb-14 px-[4%] lg:px-[10%] grid grid-cols-3 rounded-b-xl transition-all duration-300': 'hidden'}`}
                 >
-                    <li>
-                        <i className="uil uil-estate"></i> 
-                        <span className='hover-nav'>Home</span>
-                    </li>
-                </Link>
-                <Link
-                    onClick={closeMenu}
-                    activeClass="navbar--active-content"
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration={400}
-                    to="service"
-                    className="navbar--content"
-                >
-                    <li>
-                        <i className="uil uil-file-alt"></i>
-                        <span className='hover-nav'>Services</span>
-                    </li>
-                </Link>
-                <Link
-                    onClick={closeMenu}
-                    activeClass="navbar--active-content"
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration={400}
-                    to="about"
-                    className="navbar--content"
-                >
-                    <li>
-                        <i className="uil uil-user"></i>
-                        <span className='hover-nav'>About</span>
-                    </li>
-                </Link>
-                <Link
-                    onClick={closeMenu}
-                    activeClass="navbar--active-content"
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration={400}
-                    to="gallery"
-                    className="navbar--content"
-                >
-                    <li>
-                        <i className="uil uil-scenery"></i>
-                        <span className='hover-nav'>Gallery</span>
-                    </li>
-                </Link>
-                <Link
-                    onClick={closeMenu}
-                    activeClass="navbar--active-content"
-                    spy={true}
-                    smooth={true}
-                    offset={-70}
-                    duration={400}
-                    to="contact"
-                    className="navbar--content"
-                >
-                    <li>
-                        <i className="uil uil-message"></i>
-                        <span className='hover-nav'>Contact</span>
-                    </li>
-                </Link>
-            </ul>
+                    <Link
+                        onClick={closeMenu}
+                        activeClass="navbar--active-content"
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={400}
+                        to="home"
+                        className="navbar--content"
+                    >
+                        <li>
+                            <i className="uil uil-estate"></i> 
+                            <span className='hover-nav'>Home</span>
+                        </li>
+                    </Link>
+                    <Link
+                        onClick={closeMenu}
+                        activeClass="navbar--active-content"
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={400}
+                        to="service"
+                        className="navbar--content"
+                    >
+                        <li>
+                            <i className="uil uil-file-alt"></i>
+                            <span className='hover-nav'>Services</span>
+                        </li>
+                    </Link>
+                    <Link
+                        onClick={closeMenu}
+                        activeClass="navbar--active-content"
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={400}
+                        to="about"
+                        className="navbar--content"
+                    >
+                        <li>
+                            <i className="uil uil-user"></i>
+                            <span className='hover-nav'>About</span>
+                        </li>
+                    </Link>
+                    <Link
+                        onClick={closeMenu}
+                        activeClass="navbar--active-content"
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={400}
+                        to="gallery"
+                        className="navbar--content"
+                    >
+                        <li>
+                            <i className="uil uil-scenery"></i>
+                            <span className='hover-nav'>Gallery</span>
+                        </li>
+                    </Link>
+                    <Link
+                        onClick={closeMenu}
+                        activeClass="navbar--active-content"
+                        spy={true}
+                        smooth={true}
+                        offset={-70}
+                        duration={400}
+                        to="contact"
+                        className="navbar--content"
+                    >
+                        <li>
+                            <i className="uil uil-message"></i>
+                            <span className='hover-nav'>Contact</span>
+                        </li>
+                    </Link>
+                </ul>
+            </div>
 
             <div onClick={toggleNav} className='cursor-pointer lg:hidden block text-4xl'>
                 {
